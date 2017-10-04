25b290757e19a534271e7dd96c5bdd7ce56f2f15
Yandry Pozo <ypozoka@gmail.com>
2017-10-03 19:45:10 -0700

    Encrypt and decrypt in CBC model

--- set2/set2.go
+++ set2/set2.go
@@ -1,17 +1,69 @@
 package set2
 
-import "bytes"
+import (
+	"bytes"
+	"crypto/cipher"
+)
 
 func addPadding(data []byte, size int) []byte {
 	if size < 0 || size > 255 {
 		panic("size must be between 0 and 255")
 	}
 
-	pad := bytes.Repeat([]byte{byte(size)}, size)
-	result := make([]byte, len(data), len(data)+len(pad))
-
+	result := make([]byte, len(data))
 	copy(result, data)
-	result = append(result, pad...)
 
+	result = append(result, bytes.Repeat([]byte{byte(size)}, size)...)
 	return result
+}
+
+func xor(key, block []byte) []byte {
+	r := make([]byte, len(block))
+	for ix := range block {
+		r[ix] = block[ix] ^ key[ix%len(key)]
+	}
+	return r
+}
+
+func encryptCBCMode(data, initVector []byte, cb cipher.Block) []byte {
+	if mod := len(data) % cb.BlockSize(); mod != 0 {
+		data = addPadding(data, cb.BlockSize()-mod)
+	}
+
+	buff := new(bytes.Buffer)
+	section := make([]byte, cb.BlockSize())
+	prev := initVector
+
+	for ix := 0; ix < len(data); ix += cb.BlockSize() {
+		currBlock := data[ix : ix+cb.BlockSize()]
+
+		// XOR with previous block before encrypt
+		xorb := xor(prev, currBlock)
+
+		cb.Encrypt(section, xorb)
+
+		buff.Write(section)
+		prev = section
+	}
+
+	return buff.Bytes()
+}
+
+func decryptCBCModeIV(data, initVector []byte, cb cipher.Block) []byte {
+	buff := new(bytes.Buffer)
+	prev := initVector
+	section := make([]byte, cb.BlockSize())
+
+	for ix := 0; ix < len(data); ix += cb.BlockSize() {
+		currBlock := data[ix : ix+cb.BlockSize()]
+
+		// inverse order, first decrypt
+		cb.Decrypt(section, currBlock)
+
+		// xor with previous block
+		buff.Write(xor(prev, section))
+		prev = currBlock
+	}
+
+	return buff.Bytes()
 }
--- set2/set2_test.go
+++ set2/set2_test.go
@@ -2,6 +2,10 @@ package set2
 
 import (
 	"bytes"
+	"crypto/aes"
+	"encoding/base64"
+	"io/ioutil"
+	"os"
 	"testing"
 )
 
@@ -13,6 +17,53 @@ func TestAddPaddingCh9(t *testing.T) {
 	expected := []byte("YELLOW SUBMARINE\x04\x04\x04\x04")
 
 	if !bytes.Equal(r, expected) {
-		t.Fatalf("%+v", r)
+		t.Fatalf("%s", r)
 	}
+}
+
+func TestEncryptDecryptECBModeCh10(t *testing.T) {
+	const (
+		key  = "YELLOW SUBMARINE"
+		iv   = "\x00\x00\x00"
+		text = "I Fall in Love Too Easily. I fall in love too fast. I guess."
+	)
+
+	cipherAES, err := aes.NewCipher([]byte(key))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	enc := encryptCBCMode([]byte(text), []byte(iv), cipherAES)
+
+	dec := decryptCBCModeIV(enc, []byte(iv), cipherAES)
+
+	if string(dec[:len(dec)-4]) != text { // -4 coz that's the padding for text
+		t.Logf("%q != %q", string(dec), text)
+	}
+
+	t.Logf("%q", string(dec))
+}
+
+func TestECBModeCh10(t *testing.T) {
+	const key = "YELLOW SUBMARINE"
+	iv := bytes.Repeat([]byte{byte(0)}, 16)
+
+	f, err := os.Open("10.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	data, err := ioutil.ReadAll(base64.NewDecoder(base64.StdEncoding, f))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	cipherAES, err := aes.NewCipher([]byte(key))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	dec := decryptCBCModeIV(data, []byte(iv), cipherAES)
+
+	t.Logf("%v\n", string(dec))
 }
